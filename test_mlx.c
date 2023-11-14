6a4959234b6fcc7067296603788704a0261ea18e
Md Ohiduzzaman Sumon <msumon@c2r1p9.42vienna.com>
2023-11-14 18:30:14 +0100

    OK

---
+++ test_mlx.c
@@ -0,0 +1,56 @@
+#include <mlx.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	draw_square(t_data *img, int x, int y, int size, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			my_mlx_pixel_put(img, x + i, y + j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 800, 600, "So_long");
+	img.img = mlx_new_image(mlx, 800, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	draw_square(&img, 50, 50, 50, 0x00FF0000);   // Red square
+	draw_square(&img, 200, 50, 50, 0x0000FF00);  // Green square
+	draw_square(&img, 50, 200, 50, 0x000000FF);  // Blue square
+	draw_square(&img, 200, 200, 50, 0x00FFFF00); // Yellow square
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
