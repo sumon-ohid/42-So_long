0cf20e519c78bdab47958fc584d33a12d9c9e05d
Md Ohiduzzaman Sumon <msumon@c2r1p8.42vienna.com>
2023-11-17 17:12:55 +0100

    Key hook added, need to implement functions.

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 23:12:47 by msumon            #+#    #+#             */
-/*   Updated: 2023/11/17 16:29:43 by msumon           ###   ########.fr       */
+/*   Updated: 2023/11/17 16:58:35 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@
 #define WIN_HEIGHT 255
 #define MAP_HEIGHT 9
 #define CELL_SIZE 32
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
+#define KEY_ESC 65307
+
 
 typedef struct s_img
 {
@@ -42,8 +48,7 @@ typedef struct s_data
     void	*mlx_win;
 }				t_data;
 
-void	draw_wall(t_data *data, t_img *wall_img, int x, int y);
-void	draw_walls(t_data *data, t_img *wall_img, char **map);
+void	draw_walls(t_data *data, t_img *wall_img, t_img *coin_img, t_img *mario_img, t_img *exit_img, char **map);
 char	**load_map(void);
 void	draw_background(t_data *data, t_img *bg_img);
 void 	free_map(char **map);
--- srcs/wall.c
+++ srcs/wall.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:58:35 by msumon            #+#    #+#             */
-/*   Updated: 2023/11/17 16:02:52 by msumon           ###   ########.fr       */
+/*   Updated: 2023/11/17 16:53:25 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	draw_wall(t_data *data, t_img *wall_img, int x, int y)
 	mlx_put_image_to_window(data->mlx, data->mlx_win, wall_img->img, x * CELL_SIZE, y * CELL_SIZE);
 }
 
-void	draw_walls(t_data *data, t_img *wall_img, char **map)
+void	draw_walls(t_data *data, t_img *wall_img, t_img *coin_img, t_img *mario_img, t_img *exit_img, char **map)
 {
 	int	x;
 	int	y;
@@ -30,6 +30,12 @@ void	draw_walls(t_data *data, t_img *wall_img, char **map)
 		{
 			if (map[y][x] == '1')
 				draw_wall(data, wall_img, x, y);
+			else if (map[y][x] == 'C')
+				draw_wall(data, coin_img, x, y);
+			else if (map[y][x] == 'P')
+				draw_wall(data, mario_img, x, y);
+			else if (map[y][x] == 'E')
+				draw_wall(data, exit_img, x, y);
 			x++;
 		}
 		y++;
