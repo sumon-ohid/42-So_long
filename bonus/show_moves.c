c230c7be22bf4f96474d7d66d23309d5e6d62e5b
Md Ohiduzzaman Sumon <msumon@c2r1p9.42vienna.com>
2023-11-29 16:17:19 +0100

    OK

--- bonus/show_moves.c
+++ bonus/show_moves.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:11:55 by msumon            #+#    #+#             */
-/*   Updated: 2023/11/29 08:50:18 by msumon           ###   ########.fr       */
+/*   Updated: 2023/11/29 16:16:37 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 
 void	bonus_draw_text(t_data *data)
 {
-	int		x;
-	int		y;
-	int		color;
 	char	*moves;
 	char	*text;
+	int		i;
+	int		j;
 
-	x = 10;
-	y = 20;
-	color = 0x000000;
+	i = 0;
 	moves = ft_itoa(data->moves);
 	text = ft_strjoin("MOVES : ", moves);
-	mlx_string_put(data->mlx, data->mlx_win, x, y, color, text);
+	while (i < 20)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			mlx_pixel_put(data->mlx, data->mlx_win, j + 4, i + 7, 0x808080);
+			j++;
+		}
+		i++;
+	}
+	mlx_string_put(data->mlx, data->mlx_win, 10, 20, 0x000000, text);
 	free(moves);
 	free(text);
 }
